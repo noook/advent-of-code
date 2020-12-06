703227429564d4c8a75529b29d3b1ceea35ddd8c
noook <me@neilrichter.com>
2020-12-06 02:59:09 +0100

    🎨 Removed the destructuration in favor of using 1 as the initial value

--- 2020/03/index.js
+++ 2020/03/index.js
@@ -18,29 +18,27 @@ const slopes = [
   [1, 2],
 ];
 
-// Destructure to get the first value of the list as the initial value of the reduce function further
-const [firstTree, ...encounteredTrees] = slopes.map(([right, down]) => {
-  let trees = 0;
+const total = slopes
+  .map(([right, down]) => {
+    let trees = 0;
 
-  // Notice our 2 variables initiation, and the 2 variables incrementation
-  // We are walking on a map, which can be walked with coordinates like variables
-  // we start at the top left corner, and we use i to move to the bottom, and j to
-  // move to the right.
-  for (let i = 0, j = 0; i < input.length; i += down, j += right) {
-    // % resets the number of steps if we reach the end of the line.
-    // Walking 9 steps over a line of 6 boxes would come down to walking 3 steps.
-    const value = input[i][j % rowLength];
-    if (value === '#') {
-      trees++
+    // Notice our 2 variables initiation, and the 2 variables incrementation
+    // We are walking on a map, which can be walked with coordinates like variables
+    // we start at the top left corner, and we use i to move to the bottom, and j to
+    // move to the right.
+    for (let i = 0, j = 0; i < input.length; i += down, j += right) {
+      // % resets the number of steps if we reach the end of the line.
+      // Walking 9 steps over a line of 6 boxes would come down to walking 3 steps.
+      const value = input[i][j % rowLength];
+      if (value === '#') {
+        trees++
+      }
     }
-  }
 
-  return trees;
-});
-
-// This reduce acts like a list sum of numbers, except we actually multiply each number
-// between them. We use the first value of the previous list as a starting point of the reduce,
-// as starting with a 0 would always result with a 0.
-const total = encounteredTrees.reduce((acc, value) => acc * value, firstTree);
+    return trees;
+  })
+  // This reduce acts like a list sum of numbers, except we actually multiply each number
+  // between them.
+  .reduce((acc, value) => acc * value, 1);
 
 console.log(total);
