00e20449ed39e6317bf06cafa6433570fb74ee3c
noook <me@neilrichter.com>
2020-12-26 14:09:14 +0100

    🍻 Bruteforced cards with higher error range interval

--- 2020/25/index.js
+++ 2020/25/index.js
@@ -21,14 +21,16 @@ class EncryptionKey {
   }
 
   bruteforce() {
-    for (let i = 2; i < 100_000; i += 1) {
+    let i = 1;
+    let found = false;
+
+    while(!found) {
       this.subject = i;
       this.value = 1;
       this.loopSize = 0;
 
-      let found = false;
     
-      for (let j = 0; j < 100_000; j += 1) {
+      for (let j = 0; j < 10_000_000; j += 1) {
         if (this.publicKey !== this.value) {
           this.loop();
         } else {
@@ -40,7 +42,14 @@ class EncryptionKey {
       if (found) {
         return;
       }
+
+      i++;
     }
+  }
+
+  loop() {
+    this.value = (this.value * this.subject) % this.remainder;
+    this.loopSize++;
   }
 
   /**
@@ -50,16 +59,12 @@ class EncryptionKey {
    */
   solveEncryptionKey(key1, key2) {
     this.subject = key1.publicKey;
+
     for (let i = 0; i < key2.loopSize; i +=1 ) {
       this.loop();
     }
 
     return this.value;
-  }
-
-  loop() {
-    this.value = (this.value * this.subject) % this.remainder;
-    this.loopSize++;
   }
 }
 
@@ -68,6 +73,8 @@ const door = new EncryptionKey(doorKey, 20201227);
 
 card.bruteforce();
 door.bruteforce();
+
+console.log(card, door);
 
 const handshake = new EncryptionKey(null, 20201227);
 handshake.solveEncryptionKey(card, door);
